f9ee86ae9896f66abbdfb99319bbaa31d99e002b
Daniel Vidali Fryer <daniel-fryer@live.com.au>
2018-05-08 19:26:04 +1000

    covCMB_internal2
    Created covCMB_internal2 which does not perform expensive acos operation and will soon replace covCMB_internal1

--- src/CovCMBHelpers.cpp
+++ src/CovCMBHelpers.cpp
@@ -5,7 +5,9 @@ using namespace Rcpp;
 
 
 
-
+// THIS SHOULD BECOME UNUSED ONCE covCMB_internal2 IS COMPLETE
+// IT REMAINS HERE TO DO SOME BENCHMARKING
+//
 //'@title
 //'covCMB_internal1
 //'
@@ -52,6 +54,104 @@ NumericVector covCMB_internal1(Rcpp::DataFrame cmbdf, NumericVector breaks) {
       {
         if ( dist > breaks[b] ) bin++;
       }
+
+      // Add Xi*Xj to the correct bin
+      C[bin] = C[bin] + I[i]*I[j];
+
+      // Increment the number of products in the bin
+      B[bin] = B[bin] + 1;
+    }
+
+  }
+
+  // Elementwise division, normalisation
+  C = C/B;
+
+  out( _ , 0) = C;
+  out( _ , 1) = B;
+
+  return out;
+}
+
+
+
+
+//This script acknowledges that there is no need to transform with acos.
+//The breaks are cos(r_i) where r_i is the radius.
+//The bins will have equal area provided that cos(r_i) - cos(r_{i+1}) is fixed.
+//Alternatively, the bins will have equal annular width if r_{i+1} - r_i is fixed,
+//but cos(r_i) must be passed in, regardless of which
+//metric is used to fix distance beforehand.
+//We must note that: For r in (0,pi), cos is a strictly decreasing function,
+//e.g. cos(0) > cos(max.dist)
+//'@title
+//'covCMB_internal2
+//'
+//'
+//'This function acknowledges that there is no need to transform with acos.
+//'The breaks are cos(r_i) where r_i is the radius.
+//'The bins will have equal area provided that cos(r_i) - cos(r_{i+1}) is fixed.
+//'Alternatively, the bins will have equal annular width if r_{i+1} - r_i is fixed,
+//'but cos(r_i) must be passed in, regardless of which
+//'metric is used to fix distance beforehand.
+//'We must note that: For r in (0,pi), cos is a strictly decreasing function,
+//'e.g. cos(0) > cos(max.dist)
+//'
+//'
+//'
+//'@name covCMB_internal2
+//'
+//'@export
+// [[Rcpp::export]]
+NumericVector covCMB_internal2(Rcpp::DataFrame cmbdf, NumericVector cos_breaks) {
+
+  int n = cmbdf.nrow();
+  int nbreaks = cos_breaks.length();
+  int nbin = nbreaks + 1;
+  NumericVector x = cmbdf["x"];
+  NumericVector y = cmbdf["y"];
+  NumericVector z = cmbdf["z"];
+  NumericVector I = cmbdf["I"];
+  // We use nbin + 1 to create space for the variance atom (zero bin)
+  NumericVector C = NumericVector( nbin + 1 );
+  NumericVector B = NumericVector( nbin + 1 );
+  NumericMatrix out = NumericMatrix( nbin + 1, 2 ); //(nrow, ncolumn)
+
+  // Deal with the variance atom case C(0) first (i = j)
+  // These values also go into the first bin, C(1).
+  for ( int i = 0; i < n; i++ )
+  {
+    C[0] = C[0] + I[i]*I[i];
+  }
+  B[0] = n;
+
+  // Now we deal with the case i != j
+  // We only need i, j to cover the upper triangle distance matrix i < j
+  for ( int i = 0; i < n-1; i++ )
+  {
+    for ( int j = i+1; j < n; j++ )
+    {
+      // Find cosine of d(xi, xj)
+      double cos_dist = x[i]*x[j] + y[i]*y[j] + z[i]*z[j];
+
+      // Determine which bin d(xi, xj) falls into.
+      // If breaks consists of r_i rather than cos(r_i), then the
+      // output vector (except the 0 bin) will need to be reversed (in R)
+      // since cosine is a decreasing function.
+      int bin = 1;
+      for ( int b = 0; b < nbreaks; b++ )
+      {
+        // cos(r) > cos_breaks[b] iff r < breaks[b]
+        if ( cos_dist > cos_breaks[b] )
+        {
+          bin++;
+        }
+        else
+        {
+          break;
+        }
+      }
+
 
       // Add Xi*Xj to the correct bin
       C[bin] = C[bin] + I[i]*I[j];

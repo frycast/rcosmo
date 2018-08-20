d7473238724b8bfa87fc48351e4c95bec282dbce
MING <limingforlove@gmail.com>
2018-08-20 16:16:33 +1000

    New Function for Download CMB Power Spectrum

---
+++ R/DownloadCMBPS.R
@@ -0,0 +1,42 @@
+#' Download CMB Power Spectra from Planck Legacy Archive.
+#'
+#' The function \code{DownloadCMBPS} download CMB power spectra components from \url{http://pla.esac.esa.int/pla/#cosmology}.
+#' @param link  The URL to download the file
+#' @param items  Column names to specify the obtained data frame
+#' @return The CMB power spectra data frame
+#' @examples
+#' ## Case 1: Download the product paste ('COM_PowerSpect_CMB-base-plikHM-"
+#'              ,"TTTEEE-lowl-lowE-lensing-minimum-theory_R3.01.txt",sep = "") with 6 columns ("L","TT","TE","EE","BB","PP")
+#' CMBPS=DownloadCMBPS(link=paste("http://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=",
+#'                     "COM_PowerSpect_CMB-base-plikHM-","TTTEEE-lowl-lowE-lensing-minimum-theory_R3.01.txt",sep = ""),
+#'                     items=c("L","TT","TE","EE","BB","PP"))
+#' plot(CMBPS$L,CMBPS$TT, type="o",col="red",cex=0.3,
+#'      main="CMB Angular Power Spectra",xlab=expression(l),ylab=expression(paste(D[l],"(",mu,K^2,")")))
+#'
+#' ## Case 2: Download the product 'COM_PowerSpect_CMB-TT-full_R3.01.txt' with 4 columns ("L","DL","-dDL","+dDL");
+#' CMBPS=DownloadCMBPS(link=paste("http://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=",
+#'                     "COM_PowerSpect_CMB-TT-full_R3.01.txt",sep = ""),items=c("L","DL","Minus_dDL","Plus_dDL"))
+#'
+#' ## Case 3: Download the product 'COM_PowerSpect_CMB-EE-full_R3.01.txt' with 4 columns ("L","DL","-dDL","+dDL");
+#' CMBPS=DownloadCMBPS(link=paste("http://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=",
+#'                     "COM_PowerSpect_CMB-EE-full_R3.01.txt",sep = ""),items=c("L","DL","Minus_dDL","Plus_dDL"))
+#'
+#' ## Case 4: Download the product 'COM_PowerSpect_CMB-TE-full_R3.01.txt' with 4 columns ("L","DL","-dDL","+dDL");
+#' CMBPS=DownloadCMBPS(link=paste("http://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=",
+#'                     "COM_PowerSpect_CMB-TE-full_R3.01.txt",sep = ""),items=c("L","DL","Minus_dDL","Plus_dDL"))
+#'
+#' ## Case 5: Download the product 'COM_PowerSpect_CMB-low-ell-EB-full_R3.01.txt' with 3 columns ("L","DL","dDL");
+#' CMBPS=DownloadCMBPS(link=paste("http://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=",
+#'                     "COM_PowerSpect_CMB-low-ell-EB-full_R3.01.txt",sep = ""),items=c("L","DL","dDL"))
+#'
+#' ## Case 6: Download the product 'COM_PowerSpect_CMB-low-ell-BB-full_R3.01.txt' with 3 columns ("L","DL","dDL");
+#' CMBPS=DownloadCMBPS(link=paste("http://pla.esac.esa.int/pla/aio/product-action?COSMOLOGY.FILE_ID=",
+#'                    "COM_PowerSpect_CMB-low-ell-BB-full_R3.01.txt",sep = ""),items=c("L","DL","dDL"))
+#' @keywords CMB Power Spectra
+#' @references Planck Legacy Archive \url{http://pla.esac.esa.int/pla/#cosmology}
+#' @export
+#'
+DownloadCMBPS <- function(link,items){
+  CMB_PowerSpectra=read.table(link,quote="\"",col.names = items)
+  return(CMB_PowerSpectra)
+}
